009c9d5d4b4b792d190cc3b7f4ddfe198bf5d208
Steve Saylor <steven.saylor@pitt.edu>
2024-04-18 15:40:40 -0400

    fixed lint errors

--- apps/parcels-n-at/src/app/explore/page.tsx
+++ apps/parcels-n-at/src/app/explore/page.tsx
@@ -15,7 +15,7 @@ export default function Page({
   return (
     <div className="h-full w-full lg:flex lg:content-stretch">
       <div className="lg:w-1/2">
-        <NavMap selectedParcel={searchParams?.parcel} />
+        <NavMap selectedParcel={String(searchParams?.parcel)} />
       </div>
       <div className="h-full border-l-2 border-stone-400 bg-stone-200 lg:w-1/2 lg:overflow-auto">
         {searchParams?.parcel ? (
