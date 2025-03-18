6bdded6e5be9b25d41e0a4bee44f4c6f5d73f584
Steve Saylor <steven.saylor@pitt.edu>
2025-03-18 16:58:32 -0400

    add acha to owner map

--- apps/parcels-n-at/src/components/view-map.tsx
+++ apps/parcels-n-at/src/components/view-map.tsx
@@ -7,10 +7,15 @@ import { DataDrivenPropertyValueSpecification } from "maplibre-gl";
 const API_KEY = process.env.NEXT_PUBLIC_MAPTILER_API_KEY ?? "missing";
 
 const items = [
-  { id: "upitt", color: "#003594", label: "Pitt" },
+  {
+    id: "acha",
+    color: "#a6761d",
+    label: "ACHA",
+  },
   { id: "city", color: "#FFB81C", label: "City of Pittsburgh" },
   { id: "ura", color: "green", label: "URA" },
   { id: "hacp", color: "pink", label: "HACP" },
+  { id: "upitt", color: "#003594", label: "Pitt" },
   { id: "cmu", color: "#C41230", label: "CMU" },
   { id: "upmc", color: "rgb(99,11,77)", label: "UPMC" },
   { id: "highmark", color: "rgb(18,144,219)", label: "Highmark" },
@@ -34,20 +39,26 @@ export function ViewMap() {
       style={{ position: "relative" }}
       initialViewState={{ zoom: 12.5 }}
     >
-      <div className="absolute bottom-2.5 left-3.5 rounded border-2 border-black bg-white p-2">
-        <h1 className="mb-2 text-lg font-semibold leading-none">Legend</h1>
+      <div className="absolute left-3.5 top-2.5 rounded border-2 border-black border-opacity-60 bg-white/60 p-2 backdrop-blur-sm">
+        <h1 className="text-3xl font-bold leading-none">
+          Large Parcel Owners in Allegheny County
+        </h1>
+      </div>
+      <aside className="absolute bottom-2.5 left-3.5 rounded border-2 border-black bg-white/90 p-2 backdrop-blur-sm">
+        <h1 className="mb-4 text-lg font-semibold leading-none">Legend</h1>
+        <div className="mb-2 text-base font-semibold">Parcel Owner</div>
         <ul className="flex flex-col gap-2">
           {items.map((item) => (
             <li key={item.id} className="flex gap-1.5">
               <div
-                className="size-4 rounded-sm border border-black"
+                className="size-4 rounded-sm border border-black opacity-80"
                 style={{ background: item.color }}
               ></div>
-              <div>{item.label}</div>
+              <div className="text-xs font-semibold">{item.label}</div>
             </li>
           ))}
         </ul>
-      </div>
+      </aside>
 
       <Source
         type="vector"
@@ -62,7 +73,6 @@ export function ViewMap() {
           source-layer="table.large_property_owners_map.geom"
           minzoom={10}
         />
-
         <Layer
           type="line"
           source-layer="table.large_property_owners_map.geom"
