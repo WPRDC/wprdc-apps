585fba9adcbe3750729227cf76f063a895e2a163
Steve Saylor <steven.saylor@pitt.edu>
2025-05-09 12:29:28 -0400

    removed static layer

--- apps/parcels-n-at/src/app/explore/page.tsx
+++ apps/parcels-n-at/src/app/explore/page.tsx
@@ -65,7 +65,6 @@ export default async function Page({
             classes={useClasses}
             bbox={bbox}
             zoomPan={!!zoomPan}
-            availableLayers={[largeParcelPortfolios]}
           />
         </div>
 
