df4a74902cf302bcf8cc428205231e8b601b5a8a
Steve Saylor <steven.saylor@pitt.edu>
2025-12-07 11:18:52 -0500

    removed eslint next config

--- apps/parcels-n-at/next.config.js
+++ apps/parcels-n-at/next.config.js
@@ -6,9 +6,6 @@ module.exports = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   // transpilePackages: ["@wprdc/ui", "@wprdc/types"],
   images: {
     remotePatterns: [
--- apps/parcels-n-at/next.config.prod.js
+++ apps/parcels-n-at/next.config.prod.js
@@ -7,9 +7,6 @@ module.exports = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   images: {
     remotePatterns: [
       {
